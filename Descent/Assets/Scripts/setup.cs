192a5a78ae7645ebd660b223b7fc5415f1ed0d1c
JordanHooper <jordan.hooper@hotmail.co.uk>
2016-11-01 13:48:31 +0000

    Menu Updated
    - Functionality added
    - Just require initial menu system + Images of the rest of the
    characters & class gear

--- Descent/Assets/Scripts/setup.cs
+++ Descent/Assets/Scripts/setup.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using UnityEditor;
+using UnityEngine.UI;
 using System.Collections;
 
 public class setup : MonoBehaviour
@@ -11,58 +11,68 @@ public class setup : MonoBehaviour
 
     int noOfPlayers;
     bool option, champ;
-
+    public Button[] heroBut = new Button[4];
+    bool[] available = new bool[4]; 
     Hero Players = new Hero();
-
-    void Start()
-    {
-        //work out noOfPlayers;
-    }
-
-    void CreatePlayers()
-    {
-        for (int i = 0; i <= noOfPlayers; i++)
-        {
-            //Hero playerHero[i] = new Hero();
-        }
-    }
 
     public void OnOption2Select()
     {
         option = false;
     }
-
     public void OnOption1Select()
     {
         option = true;
     }
-
     public void Champ1()
     {
         champ = true;
     }
-
     public void Champ2()
     {
         champ = false;
     }
 
+    void Awake()
+    {
+        for (int i = 3; i >= 0; i--)
+        {
+            available[i] = false;
+        }
+    }
+    public void ConfirmClick()
+    {
+        for (int i = 3; i >= 0; i--)
+        {
+            if (available[i] == false)
+            {
+                heroBut[i].interactable = true;
+            }
+        }
+    }
+    public void SelectedHero()
+    {
+        for (int i = 3; i >= 0; i--)
+        {
+            if (available[i] == false)
+            {
+                heroBut[i].interactable = false;
+            }
+        }
+    }
+
     public void WarriorSelect()
     {
-
+        available[0] = true;
     }
 
     public void HealerSelect()
     {
-
+        available[1] = true;
     }
 
     public void MageSelect()
     {
-        //display the 2 mage characters
-        //choose a character isLeoric = option;
-        //choose a subclass isRune = option;
-
+        available[2] = true;
         if (champ == true)
         {
             Players.LeoricSelect(option);
@@ -75,6 +85,6 @@ public class setup : MonoBehaviour
 
     public void ScoutSelect()
     {
-
+        available[3] = true;
     }
 }
