738df94f86dd2ef37ac74af71f22ace8e80f0488
Go0dridge <luke_xbox_account@live.com>
2016-12-01 00:34:00 +0000

    Added a potential way to go about the main menu
    Go under scenes and you should see a sub folder i have made, run one of
    the scenes and click the buttons

---
+++ Descent/Assets/Scenes/Main menu scenes (theory)/LukeMenuTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LukeMenuTest : MonoBehaviour {
+
+    public string destination;
+	// Use this for initialization
+	void Start ()
+    {
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadScene ()
+    {
+        Application.LoadLevel(destination);
+    }
+}
